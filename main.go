17d6bbb50dcdc41136e3624bc8fe55fd789cdbc6
liangjunhao <3220663807@qq.com>
2024-09-10 20:16:58 +0800

    fix: get pos update

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bug-notify/handle"
 	init_tool "bug-notify/init-tool"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 )
@@ -14,15 +15,16 @@ func main() {
 	}
 	engine := gin.Default()
 	//启动一个协程用于执行binlog
-	go safelyRun(handle.Ttttt)
+	//go safelyRun(handle.Ttttt)
 	go safelyRun(handle.NotifyHandle)
-	go safelyRun(handle.TimeingTasks)
+	//go safelyRun(handle.TimeingTasks)
 	engine.Run(init_tool.Conf.ProjectConfig.Address + ":" + init_tool.Conf.ProjectConfig.Port)
 }
 
 func safelyRun(f func()) {
 	defer func() {
 		if r := recover(); r != nil {
+			fmt.Println(r)
 			zap.L().Error("数据中心异常，请联系管理员处理")
 		}
 	}()
