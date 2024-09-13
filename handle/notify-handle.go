25b1b86fc8b3702bc339d16652d67f76bd05b5d9
liangjunhao <3220663807@qq.com>
2024-09-13 09:57:13 +0800

    pref: 优化判断

--- handle/notify-handle.go
+++ handle/notify-handle.go
@@ -16,18 +16,12 @@ import (
 	"go.uber.org/zap"
 	"os"
 	"strconv"
+	"strings"
 	"sync"
 )
 
 type MyEventHandler struct {
 	canal.DummyEventHandler
-}
-
-func (h *MyEventHandler) OnRotate(header *replication.EventHeader, rotateEvent *replication.RotateEvent) error {
-	fmt.Println(header)
-	fmt.Println(rotateEvent)
-	return nil
-
 }
 
 func (h *MyEventHandler) OnPosSynced(header *replication.EventHeader, pos mysql.Position, set mysql.GTIDSet, force bool) error {
@@ -47,10 +41,15 @@ func (h *MyEventHandler) OnPosSynced(header *replication.EventHeader, pos mysql.
 
 func (h *MyEventHandler) OnRow(e *canal.RowsEvent) error {
 	action := e.Action
-	newdata := GetData(e)
+	fmt.Println("我是action", e.Action)
+	fmt.Println("我是rows", e.Rows)
+	olddata, newdata := GetData(e)
+	//根据状态判断是否发送通知，如果是update行为，关注状态是否有变化，如果没有变化就不通知
 	switch action {
+	case controller.INSERT:
+		InsertHandle(olddata)
 	case controller.UPDATE:
-		UpdateHandle(newdata)
+		UpdateHandle(olddata, newdata)
 	}
 	return nil
 }
@@ -62,17 +61,43 @@ func (h *MyEventHandler) String() string {
 func NotifyHandle() {
 	c, err := init_tool.GoMysqlConn()
 	if err != nil {
-		zap.L().Fatal("创建连接失败")
+		zap.L().Fatal("创建连接失败", zap.Error(err))
 		return
 	}
 
 	c.SetEventHandler(&MyEventHandler{})
+	//在这个地方如果不存在pos.txt文件，就创建并写入当前的最新位置
+	_, err = os.Stat(controller.POSFILENAME)
+	if err != nil {
+		if os.IsNotExist(err) {
+			masterPos, err := c.GetMasterPos()
+			if err != nil {
+				zap.L().Fatal("获取最新指针位置失败", zap.Error(err))
+				return
+			}
+			//存储文件
+			marshal, err := json.Marshal(masterPos)
+			if err != nil {
+				zap.L().Error("转换失败:", zap.Error(err))
+				return
+			}
+			err = StroageFile(string(marshal))
+			if err != nil {
+				zap.L().Error("文件写入失败:", zap.Error(err))
+				return
+			}
+		} else {
+			zap.L().Error("获取文件判断失败:", zap.Error(err))
+			return
+		}
+	}
 
 	file, err := os.ReadFile(controller.POSFILENAME)
 	if err != nil {
 		zap.L().Error("读文件失败", zap.Error(err))
-
+		return
 	}
+
 	var pos model.Potion
 	err = json.Unmarshal(file, &pos)
 	if err != nil {
@@ -85,7 +110,89 @@ func NotifyHandle() {
 	c.RunFrom(p)
 }
 
-func UpdateHandle(newdata *model.DataChanges) {
+func InsertHandle(newdata *model.DataChanges) {
+	err := Handle(newdata)
+	if err != nil {
+		zap.L().Error("插入事件处理失败", zap.Error(err))
+	}
+}
+
+func UpdateHandle(olddata, newdata *model.DataChanges) {
+	if olddata.StatusID == newdata.StatusID {
+		zap.L().Info("更新事件，状态没有变化，不做处理")
+		return
+	}
+	err := Handle(newdata)
+	if err != nil {
+		zap.L().Error("更新处理失败", zap.Error(err))
+	}
+}
+
+func GetData(e *canal.RowsEvent) (*model.DataChanges, *model.DataChanges) {
+	oldData := new(model.DataChanges)
+	oldData.ID = e.Rows[0][0].(int32)
+	oldData.ProjectID = e.Rows[0][2].(int32)
+	oldData.Subject = e.Rows[0][3].(string)
+	oldData.StatusID = e.Rows[0][7].(int32)
+	if e.Rows[0][8] == nil {
+		oldData.AssignedToID = 0
+	} else {
+		oldData.AssignedToID = e.Rows[0][8].(int32)
+	}
+	oldData.AuthorID = e.Rows[0][11].(int32)
+	newData := new(model.DataChanges)
+	if e.Action == controller.UPDATE {
+		newData.ID = e.Rows[1][0].(int32)
+		newData.ProjectID = e.Rows[1][2].(int32)
+		newData.Subject = e.Rows[1][3].(string)
+		newData.StatusID = e.Rows[1][7].(int32)
+		if e.Rows[1][8] == nil {
+			newData.AssignedToID = 0
+		} else {
+			newData.AssignedToID = e.Rows[1][8].(int32)
+		}
+		newData.AuthorID = e.Rows[1][11].(int32)
+	}
+	return oldData, newData
+}
+
+func StroageFile(data string) (err error) {
+	file, err := os.OpenFile(controller.POSFILENAME, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+	writer := bufio.NewWriter(file)
+	mu := sync.Mutex{}
+	mu.Lock()
+	_, err = writer.WriteString(data)
+	if err != nil {
+		return
+	}
+	mu.Unlock()
+	err = writer.Flush()
+	if err != nil {
+		return
+	}
+	return nil
+}
+
+// 拿到发布者和接收者用户名
+func GetUserName(takeUserID, createUserID int32) (takeName, createName string, err error) {
+	take, err := dao.GetUserInfoByUserID(takeUserID)
+	if err != nil {
+		return "", "", err
+	}
+	takeName = take.Lastname + take.Firstname
+	create, err := dao.GetUserInfoByUserID(createUserID)
+	if err != nil {
+		return takeName, "", err
+	}
+	createName = create.Lastname + create.Firstname
+	return
+}
+
+func Handle(newdata *model.DataChanges) (err error) {
 	project, err := dao.GetProject(newdata.ProjectID)
 	if err != nil {
 		zap.L().Error("获取项目失败:", zap.Error(err))
@@ -136,86 +243,44 @@ func UpdateHandle(newdata *model.DataChanges) {
 		return
 	}
 	splicingString := utils.SplicingString(phones, "@")
+	//对机器人进行签名
+	//分割字符串
+	split := strings.Split(dingdingRobot, "@")
+	var secret string
+	if len(split) >= 2 {
+		secret = split[1]
+	}
+	sign := utils.DingSecret(secret)
 	var btns []model.ActionBtns
 	btns = append(btns, model.ActionBtns{ActionURL: "dingtalk://dingtalkclient/page/link?url=" + init_tool.Conf.Redmine.URL + strconv.Itoa(int(newdata.ID)) + "&pc_slide=true", Title: "钉钉打开"})
 	btns = append(btns, model.ActionBtns{ActionURL: "dingtalk://dingtalkclient/page/link?url=" + init_tool.Conf.Redmine.URL + strconv.Itoa(int(newdata.ID)) + "&pc_slide=false", Title: "浏览器打开"})
 	data := model.SendMsg{
-		DingRobotURL: dingdingRobot,
+		DingRobotURL: split[0] + sign,
 		AtMobiles:    phones,
 		IsAtAll:      false,
 		Content: fmt.Sprintf("### <center><font color=005EFF>温馨提醒</font></center>\n"+
 			"\n--- \n"+
-			"\n> **所属项目：** <font color=#161823>%s</font>\n"+
+			"\n> **所属项目：** <font color=#000000>%s</font>\n"+
 			"\n--- \n"+
-			"\n> **任务主题：** <font color=#161823>%s</font>\n"+
+			"\n> **任务主题：** <font color=#000000>%s</font>\n"+
 			"\n--- \n"+
-			"\n> **任务状态：** <font color=#161823>%s</font>\n"+
+			"\n> **任务状态：** <font color=#000000>%s</font>\n"+
 			"\n--- \n"+
-			"\n> **创建人：** <font color=#161823>%s</font>\n"+
+			"\n> **创建人：** <font color=#000000>%s</font>\n"+
 			"\n--- \n"+
-			"\n> **处理人：** <font color=#161823>%s</font>\n"+
-			"\n--- \n"+
-			"\n <font color=005EFF>@%s</font> \n", project, newdata.Subject, status, createName, takeName, splicingString),
+			"\n> **处理人：** <font color=#000000>%s</font>\n"+
+			"\n--- \n", project, newdata.Subject, status, createName, takeName),
 		MsgType:    "actionCard",
 		Url:        init_tool.Conf.Redmine.URL + strconv.Itoa(int(newdata.ID)),
 		ActionBtns: btns,
+	}
+	if len(phones) > 0 {
+		data.Content = data.Content + fmt.Sprintf("\n <font color=005EFF>@%s</font> \n", splicingString)
 	}
 	err = api.SendMessage(data)
 	if err != nil {
 		zap.L().Error("消息发送失败:", zap.Error(err))
 		return
 	}
-}
-
-func GetData(e *canal.RowsEvent) *model.DataChanges {
-	newData := new(model.DataChanges)
-	if e.Action == controller.UPDATE {
-		newData.ID = e.Rows[1][0].(int32)
-		newData.ProjectID = e.Rows[1][2].(int32)
-		newData.Subject = e.Rows[1][3].(string)
-		newData.StatusID = e.Rows[1][7].(int32)
-		if e.Rows[1][8] == nil {
-			newData.AssignedToID = 0
-		} else {
-			newData.AssignedToID = e.Rows[1][8].(int32)
-		}
-		newData.AuthorID = e.Rows[1][11].(int32)
-	}
-	return newData
-}
-
-func StroageFile(data string) (err error) {
-	file, err := os.OpenFile(controller.POSFILENAME, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
-	if err != nil {
-		return
-	}
-	defer file.Close()
-	writer := bufio.NewWriter(file)
-	mu := sync.Mutex{}
-	mu.Lock()
-	_, err = writer.WriteString(data)
-	if err != nil {
-		return
-	}
-	mu.Unlock()
-	err = writer.Flush()
-	if err != nil {
-		return
-	}
-	return nil
-}
-
-// 拿到发布者和接收者用户名
-func GetUserName(takeUserID, createUserID int32) (takeName, createName string, err error) {
-	take, err := dao.GetUserInfoByUserID(takeUserID)
-	if err != nil {
-		return "", "", err
-	}
-	takeName = take.Lastname + take.Firstname
-	create, err := dao.GetUserInfoByUserID(createUserID)
-	if err != nil {
-		return takeName, "", err
-	}
-	createName = create.Lastname + create.Firstname
 	return
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,8 +1,37 @@
 package utils
 
-import "strings"
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"net/url"
+	"strings"
+	"time"
+)
 
 func SplicingString(str []string, s string) (newstr string) {
 	newstr = strings.Join(str, s)
+	return
+}
+
+func DingSecret(secret string) (sign string) {
+	// 1. 获取当前时间戳，单位是毫秒
+	timestamp := fmt.Sprintf("%d", time.Now().UnixNano()/1e6)
+
+	// 2. 把时间戳和密钥拼接成字符串
+	stringToSign := timestamp + "\n" + secret
+
+	// 3. 使用 HMAC-SHA256 进行加密
+	h := hmac.New(sha256.New, []byte(secret))
+	h.Write([]byte(stringToSign))
+	signData := h.Sum(nil)
+
+	// 4. 进行 Base64 编码
+	signature := base64.StdEncoding.EncodeToString(signData)
+
+	// 5. 进行 URL 编码
+	signature = url.QueryEscape(signature)
+	sign = "&timestamp=" + timestamp + "&sign=" + signature
 	return
 }
